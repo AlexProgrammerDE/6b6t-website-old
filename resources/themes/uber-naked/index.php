035ca0ecb120554c87c3412a0a296f1799bff182
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2020-06-17 11:26:27 +0200

    Fix issues

---
+++ resources/themes/uber-naked/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <title>UberGallery</title>
+    <link rel="shortcut icon" href="<?php echo THEMEPATH; ?>/images/favicon.png" />
+
+    <link rel="stylesheet" type="text/css" href="<?php echo THEMEPATH; ?>/style.css" />
+    <?php echo $gallery->getColorboxStyles(1); ?>
+
+    <script type="text/javascript" src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+    <?php echo $gallery->getColorboxScripts(); ?>
+
+    <?php file_exists('googleAnalytics.inc') ? include('googleAnalytics.inc') : false; ?>
+</head>
+
+<body>
+
+    <h1>UberGallery</h1>
+
+    <?php
+        $galleryArray['relText'] = 'colorbox';
+        echo $gallery->readTemplate('templates/defaultGallery.php', $galleryArray);
+    ?>
+
+</body>
+
+</html>
