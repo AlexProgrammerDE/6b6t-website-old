50b8d9215c80b75abacc68e8c539c10ddb19d749
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2020-06-17 15:29:52 +0200

    Customize upload page

--- upload.php
+++ upload.php
@@ -1,15 +1,13 @@
 <!DOCTYPE HTML>
 <html>
 <head>
+<link rel="stylesheet" type="text/css" href="css/global.css">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>UploadiFive Test</title>
+<title>6b6t - Upload</title>
 <script src="resources-uploadify/jquery.min.js" type="text/javascript"></script>
 <script src="resources-uploadify/jquery.uploadifive.js" type="text/javascript"></script>
 <link rel="stylesheet" type="text/css" href="resources-uploadify/uploadifive.css">
 <style type="text/css">
-body {
-	font: 13px Arial, Helvetica, Sans-serif;
-}
 .uploadifive-button {
 	float: left;
 	margin-right: 10px;
@@ -21,11 +19,24 @@ body {
 	margin-bottom: 10px;
 	padding: 0 3px 3px;
 	width: 300px;
+	background-color: #ffffff;
 }
 </style>
 </head>
 
 <body>
+    <header class="header">
+        <div class="container">
+            <ul style="overflow: hidden; list-style-type: none; margin: 0; padding: 0;">
+				<li class="logo"><a href="index.html"><img src="img/logo.png" alt="logo" class="logo hvr-grow" height="64px" width="64px"></a></li>
+				<li class="navbutton">Online: <span data-playercounter-ip="6b6t.org">0</span> / 9000</li>
+				<a class="nav" href="upload.php"><li class="navbutton">Upload</li></a>
+				<a class="nav" href="gallery.php"><li class="navbutton">Gallery</li></a>
+				<a class="nav" href="index.html"><li class="navbutton">Home</li></a>
+            </ul>                     
+        </div>
+    </header>
+
 	<h1>UploadiFive Demo</h1>
 	<form>
 		<div id="queue"></div>
