08d85c75931d99f4bda32d597fe4fdd4fb77d8db
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-02-13 16:50:50 +0100

    New format for smaller devices

--- pages/commands.tsx
+++ pages/commands.tsx
@@ -8,23 +8,28 @@ const Home: NextPage = () => {
             {
                 commands.map((category, index) => {
                     return (
-                        <section key={index} className="mb-2">
+                        <section key={index} className="mb-3">
                             <h3>{category.name}</h3>
                             <div className="flex flex-col gap-1">
                                 {
                                     category.commands.map((command, index) => {
                                         return (
                                             <div className="flex flex-row gap-1" key={index}>
-                                                <code className="bg-gray-800 rounded-md p-0.5">
-                                                    {command.name}
-                                                </code>
-                                                {
-                                                    command.limited &&
-                                                    <>
-                                                        {" - "}
-                                                        <p className="font-bold text-purple-600">{command.limited.name}</p>
-                                                    </>
-                                                }
+                                                <div className="flex flex-col md:flex-row gap-1">
+                                                    <div className="flex flex-col">
+                                                        <code
+                                                            className="bg-gray-800 rounded-md p-0.5 min-h-0 overflow-x-scroll flex-grow-0">
+                                                            {command.name}
+                                                        </code>
+                                                    </div>
+                                                    {
+                                                        command.limited &&
+                                                        <>
+                                                            <div className="hidden md:block">{" - "}</div>
+                                                            <p className="font-bold text-purple-600">{command.limited.name}</p>
+                                                        </>
+                                                    }
+                                                </div>
                                                 {" - "}
                                                 <p>{command.description}</p>
                                             </div>
@@ -32,7 +37,6 @@ const Home: NextPage = () => {
                                     })
                                 }
                             </div>
-
                         </section>
                     )
                 })
