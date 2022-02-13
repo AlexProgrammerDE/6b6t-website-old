1aa410dfeec65cb69beb7b9c607b77a406759a8f
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-02-13 18:49:36 +0100

    Add cloudflare insights

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,6 +9,11 @@ export default function Layout({children}: {children: ReactNode}) {
     return (
         <>
             <SEOHeader/>
+            <Script id="crate" src="https://static.cloudflareinsights.com/beacon.min.js"
+                    strategy="afterInteractive"
+                    data-cf-beacon='{"token": "695b549799c6448d9b80e25f5e5d1996"}'
+            >
+            </Script>
             <Script id="crate" src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3"
                     strategy="lazyOnload"
             >
